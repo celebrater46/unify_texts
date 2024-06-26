6fc8d679a06de902911656702f35ddf894c8200f
celebrater46 <tkgm45nbchk@gmail.com>
2024-06-26 15:36:33 +0900

    test succeeded

--- unify_texts.py
+++ unify_texts.py
@@ -1,18 +1,35 @@
-import docx
+import os
 
-def say_hello():
-    print("Hello world!!!!!!!!!")
+# def create_txt(doc):
+#     f = open("files/converted.txt", "w", encoding='utf-8')
+#     for paragraph in doc.paragraphs:
+#         f.write(paragraph.text + "\n")
+#     f.close()
 
-say_hello()
+def get_filenames():
+    dir_path = "texts"
+    file_names = [
+        f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))
+    ]
+    return file_names
 
-def create_txt(doc):
-    f = open("files/converted.txt", "w", encoding='utf-8')
-    for paragraph in doc.paragraphs:
-        f.write(paragraph.text + "\n")
+def get_text(file_name):
+    f = open(f"texts/{file_name}", "r", encoding='utf-8')
+    text = f.read()
+    f.close()
+    return text
+
+def create_unified_text(txt):
+    f = open("unified.txt", "w", encoding='utf-8')
+    f.write(txt)
     f.close()
 
-def open_docx():
-    document = docx.Document("files/test.docx")
-    create_txt(document)
+def unify_texts():
+    tempstr = ""
+    fs = get_filenames()
+    for f in fs:
+        tempstr += get_text(f)
+        tempstr += "\n\n####################[PAGE_BREAK]####################\n\n"
+    create_unified_text(tempstr)
 
-open_docx()
+unify_texts()
